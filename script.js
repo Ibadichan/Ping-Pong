5aaa1640df202d38fcea24bf3b7100b2a2533c07
Ivan Badicean <badican01117@gmail.com>
2018-03-14 14:19:37 +0100

    lesson-3, bounce off wall

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ var ctx = canvas.getContext("2d");
 
 var x = canvas.width / 2, y = canvas.height - 30;
 var dx = 2, dy = -2;
+var ballRadius = 10;
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
@@ -15,6 +16,10 @@ function drawBall() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) { dx = -dx }
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) { dy = -dy }
+
   x += dx;
   y += dy;
 }
