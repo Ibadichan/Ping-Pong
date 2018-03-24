7a27c570cb0160c0b6fb3d5d212188ee4bd57a55
Ivan Badicean <badican01117@gmail.com>
2018-03-24 12:44:56 +0100

    lesson-6, drow bricks

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var canvas = document.getElementById("myCanvas"), ctx = canvas.getContext("2d");
-var x = canvas.width / 2, y = canvas.height - 30, dx = 2, dy = -2, ballRadius = 10;
-var paddleHeight = 10, paddleWidth = 75, paddleX = (canvas.width - paddleWidth) / 2;
+var x = canvas.width / 2, y = canvas.height - 30, dx = 2, dy = -2, ballRadius = 8;
+var paddleHeight = 7, paddleWidth = 60, paddleX = (canvas.width - paddleWidth) / 2;
 var rightPressed = false, leftPressed = false;
+var brickRowCount = 3, brickColumnCount = 5, brickPadding = 5, bricks = [];
+var brickWidth = 50, brickHeight = 10, brickOffsetTop = 15, brickOffsetLeft = 15;
+
+for(var column = 0; column < brickColumnCount; column++) {
+  bricks[column] = [];
+  for(var row = 0; row < brickRowCount; row++) { bricks[column][row] = { x: 0, y: 0 } }
+}
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -32,9 +39,26 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawBricks() {
+  for(var column = 0; column < brickColumnCount; column++) {
+    for(var row = 0; row < brickRowCount; row++) {
+      var brickX = (column * (brickWidth + brickPadding)) + brickOffsetLeft;
+      var brickY = (row * (brickHeight + brickPadding)) + brickOffsetTop;
+      bricks[column][row].x = brickX;
+      bricks[column][row].y = brickY;
+      ctx.beginPath();
+      ctx.rect(brickX, brickY, brickWidth, brickHeight);
+      ctx.fillStyle = "#0095DD";
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawBricks();
   drawPaddle();
 
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) { dx = -dx }
@@ -49,11 +73,10 @@ function draw() {
     }
   }
 
-  if (rightPressed && paddleX < canvas.width - paddleWidth) { paddleX += 7 }
-  else if (leftPressed && paddleX > 0) { paddleX -= 7 }
+  if (rightPressed && paddleX < canvas.width - paddleWidth) { paddleX += 5 }
+  else if (leftPressed && paddleX > 0) { paddleX -= 5 }
 
-  x += dx;
-  y += dy;
+  x += dx, y += dy;
 }
 
 var game = setInterval(draw, 20);
